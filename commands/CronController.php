c78317ca110851e2d285e093c23a233f665f3191
Yusufjon Ismatov <iyusufjon19@gmail.com>
2024-08-06 10:50:45 +0500

    backup test

--- commands/CronController.php
+++ commands/CronController.php
@@ -27,6 +27,29 @@ class CronController extends Controller
      */
     public function actionBackup($message = 'hello cron backup')
     {
+        $date = '';
+        $filename_sql = 'data/backup_postgres' . $date . '.sql';
+        $filename_tar = 'data/backup_postgres' . $date . '.tar';
+
+        echo 'filename sql: ' . $filename_sql . "\n";
+        echo 'filename tar: ' . $filename_tar . "\n";
+
+//        $command = 'mysqldump -u root -p'.Yii::$app->db->password.' bts > '.$filename_sql;
+        $command = 'PGPASSWORD="' . Yii::$app->db->password . '" pg_dump -U postgres -h localhost backupdb > ' . $filename_sql;
+
+        echo 'command: ' . $command . "\n";
+        $output = shell_exec($command);
+
+        $command = 'tar -czvf ' . $filename_tar . ' ' . $filename_sql;
+        echo 'command: ' . $command . "\n";
+        $output = shell_exec($command);
+
+        if (is_file($filename_tar)) {
+//            $command = 'sshpass -p "bts@202011192009#" scp -r -P 2208 '.Yii::getAlias('@app').'/'.$filename_tar.' adham@83.221.163.9:/home/adham/data';
+//            echo 'command: '.$command. "\n";
+//            $output = shell_exec($command);
+        }
+
         echo $message . "\n";
 
         return ExitCode::OK;
