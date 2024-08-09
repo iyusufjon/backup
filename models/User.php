9388ebff70a96498fc544058a77cff79d697b378
Yusufjon Ismatov <iyusufjon19@gmail.com>
2024-08-09 11:38:42 +0500

    username and password change

--- models/User.php
+++ models/User.php
@@ -13,15 +13,15 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     private static $users = [
         '100' => [
             'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin@$',
+            'username' => 'tulkin',
+            'password' => '@das#@#@S$',
             'authKey' => 'test100key',
             'accessToken' => '100-token',
         ],
         '101' => [
             'id' => '101',
             'username' => 'demo',
-            'password' => 'demo@$',
+            'password' => 'demo@$213@!#',
             'authKey' => 'test101key',
             'accessToken' => '101-token',
         ],
